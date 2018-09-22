da0515f19ff380f24108e53dde055a74b44f8309
dtmkeng <35913@schoolptk.ac.th>
2018-09-23 03:03:42 +0700

    basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { InputInsertComponent } from './input-insert/input-insert.component';
+import { ShowInputDataComponent } from './show-input-data/show-input-data.component';
+
+import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { RoutingMainModule } from "./routing-main/routing-main.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputInsertComponent,
+    ShowInputDataComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    RoutingMainModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routing-main/routing-main.module.spec.ts
@@ -0,0 +1,13 @@
+// import { RoutingMainModule } from "./RoutingMainModule";
+
+// describe('RoutingMainModule', () => {
+//   let routingMainModule: RoutingMainModule;
+
+//   beforeEach(() => {
+//     routingMainModule = new RoutingMainModule();
+//   });
+
+//   it('should create an instance', () => {
+//     expect(routingMainModule).toBeTruthy();
+//   });
+// });
